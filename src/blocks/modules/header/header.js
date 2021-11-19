e04c85fde9d3bb36284ddeb333246c8906abb911
S19S93 <S19S93@yandex.ru>
2021-11-19 17:51:43 +0200

    Master

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,17 +1,14 @@
-// (function fixedMenuScroll() {
-//     const header = document.querySelector('.header');
-//     const logo = document.querySelector('.logo');
+(function fixedMenuScroll() {
+    const header = document.querySelector('.header');
 
-//     if (header && logo) {
-//         window.addEventListener('scroll', function () {
-//             if (window.scrollY > 0) {
-//                 header.classList.add('header--fixed');
-//                 logo.classList.add('logo--small');
+    if (header) {
+        window.addEventListener('scroll', function () {
+            if (window.scrollY > 0) {
+                header.classList.add('header--fixed');
 
-//             } else {
-//                 header.classList.remove('header--fixed');
-//                 logo.classList.remove('logo--small');
-//             }
-//         })
-//     }
-// })();
+            } else {
+                header.classList.remove('header--fixed');
+            }
+        })
+    }
+})();
